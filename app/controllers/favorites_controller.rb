07ff6f4e3fbd01c6572242910ab49f050bec5cd7
Naoto Eguchi <naoto725@gmail.com>
2018-06-05 17:10:50 +0000

    icappを更新

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+   
    def create
     favorite = current_user.favorites.create(ic_id: params[:ic_id])
     redirect_to ics_url, notice: "#{favorite.ic.user.name}さんの投稿をお気に入り登録しました"
@@ -8,4 +9,11 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(ic_id: params[:ic_id]).destroy
     redirect_to ics_url, notice: "#{favorite.ic.user.name}さんの投稿をお気に入り解除しました"
   end
+  
+   def show
+     @user = User.find(params[:id])
+     # @favorite_ics = current_user.favorite_ics.find_by(user_id: @user.id)
+     @favorite_ics = @user.favorite_ics 
+   end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: "tops#top"
   resources :sessions, only: [:new, :create, :destroy]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :show]
   resources :users, only: [:new, :create, :show]
   resources :ics do
     collection do
