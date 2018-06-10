ea526580b44857fe1c8b05bfbe71a73f4fa6aeb7
Naoto Eguchi <naoto725@gmail.com>
2018-06-10 09:23:49 +0000

    課題の修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180603150927) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "ic_id"
